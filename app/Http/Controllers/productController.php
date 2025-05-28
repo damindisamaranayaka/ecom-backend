9ea434f4fbf9f2a48a067f8798e1ad765161119d
Damindi Samaranayaka <damindisamaranayaka2@gmail.com>
2025-05-28 11:06:21 +0530

    Refactor addProduct method to save product details and file path

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\product;
 class productController extends Controller
 {
     //
     function addProduct(Request $req)
     {
-        return $req->file('file')->store('products'); //products is a folder name which file has been saved
+       // return $req->file('file')->store('products'); //products is a folder name which file has been saved
+       $product= new product;
+       $product->name=$req->input('name');
+       $product->price=$req->input('price');
+       $product->description=$req->input('description');
+       $product->filePath=$req->file('file')->store('products');
+       $product->save();
+       return $product;    
     }
 }
